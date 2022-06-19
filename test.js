4ed9c3bd952b5aa4fcc37fb40009b53ea35a7e5e
Eriniss <jhs2195@gmail.com>
2022-06-19 20:07:09 +0900

    4.final

--- test.js
+++ test.js
@@ -1,25 +1,12 @@
-document.querySelector(".left-arrow").onclick = function() {
-    var currentSlide = document.querySelector("#photo .slide.active");
-    var previousSlide = currentSlide.previousElementSibiling;
-    if (previousSlide === null) {
-        previousSlide = currentSlide.parentElement.firstElementChild;
-    }
-    currentSlide.animate({
-        opacity: [1, 0]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both",
-    })
-    currentSlide.classList.remove("active");
-    previousSlide.animate({
-        opacity: [0, 1]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both",
-    })
-    previousSlide.classList.add("active");
+function reverse(str) {
+  for(var i = str.length-1; i >= 0; i--) {
+      var reverStr = "";
+      reverStr = reverStr + str.charAt(i);
+  }
+  return reverStr;
 }
+
+
+
+// 채점을 위한 코드입니다.
+document.write(reverse("Nice to meet you"));
